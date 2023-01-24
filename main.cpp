a48686153b8efe38ce0aa6f1e8df106686001e17
Trey <trey@morris2morris.com>
2023-01-24 14:05:55 -0500

    Add Binary to Two's Compliment Method
    - Takes in a Binary String and does two's compliment (compliment of binary bit plus one bit)

--- main.cpp
+++ main.cpp
@@ -5,16 +5,19 @@ int HexToDec(string n) { return stoi(n, 0, 16); }
 void decimalToBinary(int n);
 void decimaltoHex(int n);
 void binaryToDecimal(char binaryNum[64]);
+string twosCompliment(string str);
 int main()
 {
     int userInput;
     int numberInput = 0;
+    string binaryString;
     char userChar[64];
     string hexNumber;
     cout << "1. Decimal to Binary: " << endl;
     cout << "2. Decimal to Hex: " << endl;
     cout << "3. Binary to Decimal: " << endl;
     cout << "4. Hex to Decimal: " << endl;
+    cout << "5. Binary to Two's Compliment: " << endl;
     cout << "Choose an option: ";
     cin >> userInput;
     switch(userInput)
@@ -39,6 +42,11 @@ int main()
             cout << "Input a Hex Number: ";
             cin >> hexNumber;
             cout << HexToDec(hexNumber);
+            break;
+        case 5:
+            cout<< "Input a binary string ";
+            cin >> binaryString;
+            cout << twosCompliment(binaryString);
             break;
         default:
         cout << "Invalid user input";
@@ -75,4 +83,36 @@ void binaryToDecimal(char binaryNum[])
     cout << stoi(binaryNum, 0, 2);
     cout << " " << endl;
     cout << "Time complexity is: O(n) where n is the length of the given string";
+}
+
+string twosCompliment(string str)
+{
+    int n = str.length();
+    int i;
+    for (i = n - 1; i >= 0; i--)
+    {
+        if (str[i] == '1')
+        {
+            break;
+        }
+    }
+    if (i == -1)
+    {
+        return '1' + str;
+    }
+    for (int k = i - 1; k >= 0; k--)
+    {
+        
+        if (str[k] == '1')
+        {
+            str[k] = '0';
+        }
+        else
+        {
+            str[k] = '1';
+        }
+    }
+    cout << "Time complexity is: O(n)" << endl;
+    cout << "The Two's Compliment of is ";
+    return str;
 }
