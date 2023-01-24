3e4cf7d6e828198010bec5a6451141f9d98f4a30
Trey <trey@morris2morris.com>
2023-01-24 14:26:59 -0500

    Update 01-24-23 14:26
    -Put all conversion methods into a Conversions class in order to include OOP.
    -Next Commit update should hopefully include Error and Exception Handling

--- main.cpp
+++ main.cpp
@@ -1,13 +1,76 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-int HexToDec(string n) { return stoi(n, 0, 16); }
-void decimalToBinary(int n);
-void decimaltoHex(int n);
-void binaryToDecimal(char binaryNum[64]);
-string twosCompliment(string str);
+class Conversions
+{
+public:
+    int HexToDec(string n) { return stoi(n, 0, 16); }
+    void decimalToBinary(int n)
+    {
+        cout << "Converting decimal number " << n << " to binary " << endl;
+        // Assumption that the integer is 32 bits
+        for (int i = 31; i >= 0; i--)
+        {
+            int k = n >> i;
+            if (k & 1)
+            {
+                cout << "1";
+            }
+            else
+            {
+                cout << "0";
+            }
+        }
+        cout << " " << endl;
+        cout << "Time complexity is: O(1)";
+    }
+    void decimaltoHex(int n)
+    {
+        // Cheating again and using a predefined function
+        cout << hex << n << endl;
+    }
+    void binaryToDecimal(char binaryNum[])
+    {
+        cout << stoi(binaryNum, 0, 2);
+        cout << " " << endl;
+        cout << "Time complexity is: O(n) where n is the length of the given string";
+    }
+
+    string twosCompliment(string str)
+    {
+        int n = str.length();
+        int i;
+        for (i = n - 1; i >= 0; i--)
+        {
+            if (str[i] == '1')
+            {
+                break;
+            }
+        }
+        if (i == -1)
+        {
+            return '1' + str;
+        }
+        for (int k = i - 1; k >= 0; k--)
+        {
+
+            if (str[k] == '1')
+            {
+                str[k] = '0';
+            }
+            else
+            {
+                str[k] = '1';
+            }
+        }
+        cout << "Time complexity is: O(n)" << endl;
+        cout << "The Two's Compliment of is ";
+        return str;
+    }
+};
 int main()
 {
+    Conversions conversions;
     int userInput;
     int numberInput = 0;
     string binaryString;
@@ -25,94 +88,30 @@ int main()
         case 1:
             cout << "Input an integer: ";
             cin >> numberInput;
-            decimalToBinary(numberInput);
+            conversions.decimalToBinary(numberInput);
             break;
         case 2:
             cout << "Input an integer: ";
             cin >> numberInput;
-            decimaltoHex(numberInput);
+            conversions.decimaltoHex(numberInput);
             break;
         case 3:
             cout << "Input a binary number: ";
             cin >> userChar;
-            binaryToDecimal(userChar);
+            conversions.binaryToDecimal(userChar);
             break;
         case 4:
             //Cheating a little and using a predefined function
             cout << "Input a Hex Number: ";
             cin >> hexNumber;
-            cout << HexToDec(hexNumber);
+            cout << conversions.HexToDec(hexNumber);
             break;
         case 5:
             cout<< "Input a binary string ";
             cin >> binaryString;
-            cout << twosCompliment(binaryString);
+            cout << conversions.twosCompliment(binaryString);
             break;
         default:
         cout << "Invalid user input";
     }
-}
-
-void decimalToBinary(int n)
-{
-    cout << "Converting decimal number " << n << " to binary " << endl;
-    //Assumption that the integer is 32 bits
-    for (int i = 31; i >= 0; i--)
-    {
-        int k = n >> i;
-        if (k & 1)
-        {
-            cout << "1";
-        }
-        else
-        {
-            cout << "0";
-        }
-        
-    }
-    cout << " " << endl;
-    cout << "Time complexity is: O(1)";
-}
-void decimaltoHex(int n)
-{
-    //Cheating again and using a predefined function
-    cout << hex << n << endl;
-}
-void binaryToDecimal(char binaryNum[])
-{
-    cout << stoi(binaryNum, 0, 2);
-    cout << " " << endl;
-    cout << "Time complexity is: O(n) where n is the length of the given string";
-}
-
-string twosCompliment(string str)
-{
-    int n = str.length();
-    int i;
-    for (i = n - 1; i >= 0; i--)
-    {
-        if (str[i] == '1')
-        {
-            break;
-        }
-    }
-    if (i == -1)
-    {
-        return '1' + str;
-    }
-    for (int k = i - 1; k >= 0; k--)
-    {
-        
-        if (str[k] == '1')
-        {
-            str[k] = '0';
-        }
-        else
-        {
-            str[k] = '1';
-        }
-    }
-    cout << "Time complexity is: O(n)" << endl;
-    cout << "The Two's Compliment of is ";
-    return str;
 }
